c340b463a938598144458210c59690a91a7990b2
alexzwir <alexzwir@gmail.com>
2018-11-11 12:48:03 -0200

    Adding some bootstrap style and UX to the page

--- website/core/forms.py
+++ website/core/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class EloUrlToBeShorten(forms.Form):
-    elo_url_to_be_shorten = forms.CharField(max_length=1000,label="",widget=forms.TextInput(attrs={'id':'url_parcial_input','type':'text','name':'url_parcial_input','value':''}))
+    elo_url_to_be_shorten = forms.CharField(max_length=1000,label="",widget=forms.TextInput(attrs={'id':'url_parcial_input','type':'text','name':'url_parcial_input','value':'','class':'form-control'}))
     
